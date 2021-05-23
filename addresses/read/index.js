61dc9da0f3de2a09c4a899af4bff3399a966df8c
PJSimon <psimon@box.com>
2021-05-22 20:42:42 -0700

    Initial Setup

--- addresses/read/index.js
+++ addresses/read/index.js
@@ -2,23 +2,23 @@ const data = require('@begin/data')
 
 exports.handler = async function read(req) {
   let pages = await data.get({
-    table: 'todos',
+    table: 'addresses',
     limit: 25
   })
 
-  let todos = []
-  for await (let todo of pages) {
-    todos.push(todo)
+  let addresses = []
+  for await (let address of pages) {
+    addresses.push(address)
   }
 
-  todos.sort((a, b) => a.created - b.created)
+  addresses.sort((a, b) => a.zip - b.zip)
 
   return {
-    statusCode: 201,
+    statusCode: 200,
     headers: {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body: JSON.stringify(todos)
+    body: JSON.stringify(addresses)
   }
 }
---
+++ search/execute/index.js
@@ -0,0 +1,25 @@
+let arc = require('@architect/functions')
+let data = require('@begin/data')
+
+exports.handler = async function execute(req) {
+  let query = arc.http.helpers.bodyParser(req)
+
+  let pages = await data.get({
+    table: 'partialAddresses',
+    limit: 25
+  })
+
+  let addresses = []
+  for await (let address of pages) {
+    addresses.push(address)
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify(addresses)
+  }
+}
