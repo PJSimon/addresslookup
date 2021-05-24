3c5bf887306e113a5bb834ec15a9a04682dd71c3
Patrick <patrick@hirescale.io>
2021-05-23 21:41:33 -0700

    Fix Delete

---
+++ todos/destroy/index.js
@@ -0,0 +1,19 @@
+let arc = require('@architect/functions')
+let data = require('@begin/data')
+
+exports.handler = async function destroy(req) {
+  console.log(req.body)
+
+  let key = arc.http.helpers.bodyParser(req).key
+  await data.destroy({
+    key,
+    table: 'todos'
+  })
+  return {
+    statusCode: 204,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    }
+  }
+}
