a4fe52f4a85ebfb1d5850ebec79c9e9087393123
Patrick Simon <pjsimon4321@gmail.com>
2021-05-24 20:58:39 -0700

    Update index.js

--- search/execute/index.js
+++ search/execute/index.js
@@ -77,6 +77,7 @@ exports.handler = async function execute(req) {
 
   // Output Results
   console.log('Output Results')
+  console.log(queryObjects)
   let pages = await data.get(queryObjects)
 
   let addresses = []
