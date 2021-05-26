17d6a2912a3e0482bcce23f06d8926b4c9e69ec9
Patrick <patrick@hirescale.io>
2021-05-25 22:54:13 -0700

    Add Delete All

---
+++ addresses/destroyAll/index.js
@@ -0,0 +1,52 @@
+let arc = require('@architect/functions')
+let data = require('@begin/data')
+
+exports.handler = async function destroyAll(req) {
+
+  let pages = {}
+
+  do {
+    pages = await data.get({
+      table: 'addresses',
+      limit: 25
+    })
+
+    let addresses = []
+    for await (let address of pages) {
+      await data.destroy({
+          address.key,
+          table: 'addresses'
+      })
+    }
+  } while (pages != null)
+
+  do {
+      pages = await data.get({
+        table: 'partialAddresses',
+        limit: 25
+      })
+
+      let addresses = []
+      for await (let address of pages) {
+        await data.destroy({
+            address.key,
+            table: 'partialAddresses'
+        })
+      }
+    } while (pages != null)
+
+
+
+  let key = arc.http.helpers.bodyParser(req).key
+  await data.destroy({
+    key,
+    table: 'addresses'
+  })
+  return {
+    statusCode: 204,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    }
+  }
+}
