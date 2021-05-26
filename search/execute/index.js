5bad28505f78cc4b0153b4535b57e045efe989ca
Patrick <patrick@hirescale.io>
2021-05-26 14:31:04 -0700

    Fix bug when no search results

--- search/execute/index.js
+++ search/execute/index.js
@@ -61,7 +61,7 @@ exports.handler = async function execute(req) {
         'content-type': 'application/json; charset=utf8',
         'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
       },
-      body: ''
+      body: '[]'
     }
   }
 }
